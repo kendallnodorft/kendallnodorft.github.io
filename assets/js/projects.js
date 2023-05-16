2d43921f296c5d9bfc194079544a368c5267d957
kendallnodorft <128556711+kendallnodorft@users.noreply.github.com>
2023-05-15 22:21:17 -0500

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -85,7 +85,7 @@ let render_projects = (slug) => {
         
         {
             image: 'assets/images/siroaksalot.png',
-            link: 'https://raw.githubusercontent.com/kendallnodorft/kendallnodorft.github.io/main/assets/images/siroaksalot.jpg',
+            link: 'https://github.com/kendallnodorft/kendallnodorft.github.io/blob/b711a6a5e0790e5afbaaaac09d7db96abaa536a9/assets/images/siroaksalot.jpg',
             title: 'Renaissance Dog Portrait',
             demo: false,
             technologies: ['Graphic Design', 'Photoshop'],
